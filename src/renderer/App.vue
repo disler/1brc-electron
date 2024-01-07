00689dcc062a49503e95c8d8d93721f8163e6dd2
qwukker <minor7addfloortom@gmail.com>
2024-01-07 10:43:55 -0600

    Define Pagination and BrcRow interfaces, remove unused code, and update the page title in App.vue.

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,32 +1,23 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+interface Pagination {
+  page: number;
+  itemsPerPage: number;
+  table: string;
+}
 
-window.electronAPI.sendMessage("Hello from App.vue!");
+interface BrcRow {
+  station: string;
+  min: number;
+  mean: number;
+  max: number;
+  measurement: string;
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>1 Billion Row Challenge - Electron Edition</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
